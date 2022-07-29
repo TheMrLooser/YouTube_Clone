63fa1b7286221cbda1a16bc6f4e682b04b9feb7d
TheMrLooser <ankushsoni7171@gmail.com>
2022-07-29 15:11:52 +0530

    removed firebased keys

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth , GoogleAuthProvider} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwyqcf3DHlc3ggxcMxzXVxyFrNNU2lBoU",
-  authDomain: "video-78129.firebaseapp.com",
-  projectId: "video-78129",
-  storageBucket: "video-78129.appspot.com",
-  messagingSenderId: "277189547265",
-  appId: "1:277189547265:web:28d9674f45d0fdd7a17700"
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " "
 };
 
 // Initialize Firebase
